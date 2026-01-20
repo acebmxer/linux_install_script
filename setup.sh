d3a43e4badbec9602338dc6f3ac46de6ae7b1a70
acebmxer <nick.pozza1@gmail.com>
2026-01-20 11:13:29 -0500

    Add spacing in menu display for better readability

--- setup.sh
+++ setup.sh
@@ -96,7 +96,10 @@ printc() {
 # -------------------------------------------------------------
 clear
 printc "$HEADER" "A log file as has been created "$log_file" the 5 most recent logs will be kept."
+printc "$HEADER" " "
+printc "$HEADER" " "
 printc "$HEADER" "To begin select of 1 of 7 options."
+printc "$HEADER" " "
 printc "$OPTION" "1.  To fully upgrade the system and Install xen-guest-utilities."
 printc "$OPTION" "2.  To fully upgrade the system with Topgrade and Install xen-guest-utilities."
 printc "$OPTION" "3.  To install Docker"
